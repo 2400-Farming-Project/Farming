d099def800f42cfb28e343bc3051bcb1865cddcf
boop213 <150979904+boop213@users.noreply.github.com>
2024-04-20 21:48:26 -0700

    Update ArrayListMethods.java

--- ArrayListMethods.java
+++ ArrayListMethods.java
@@ -2,7 +2,20 @@ import java.util.ArrayList;
 
 
 public class ArrayListMethods {
-
-
+private static final int MAX_ITEMS = 100;
   
+private static String[] itemNames = new String[MAX_ITEMS];
+private static int[] quantityOfItem = new int[MAX_ITEMS];
+  
+  
+public static void addItem(String itemName, int quantity) {
+  for (int i = 0; i < itemNames.length; i++) {
+  if (itemNames[i] == null) {
+  itemNames[i] = itemName;
+    quantityOfItem[i] = quantity;
+    System.out.println( itemName + "added to the catalog with quantity" + quantity + ".");
+    return();
+    }
+  }
+  System.out.println("Catalog is full");
 }
