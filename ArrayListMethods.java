828a18b33e35db1ee084bb16d67280f76d3e2e1e
Monsterous3110 <158372960+Monsterous3110@users.noreply.github.com>
2024-04-21 19:39:04 -0700

    Update ArrayListMethods.java
    Implemented removeItem() method

--- ArrayListMethods.java
+++ ArrayListMethods.java
@@ -21,7 +21,21 @@ public static void addItem(String itemName, int quantity) {
 }
 
 public static void removeItem(String itemName, int quantity) {
-  
+  for (int i = 0; i < itemNames.length; i++) {
+    if (itemName.equals(itemNames[i])) {
+      quantityOfItem[i] -= quantity;
+      if (quantityOfItem[i]<0){
+        quantityOfItem[i] = 0;
+      }
+      if (quantityOfItem[i] <= 0) {
+        itemNames[i] = null;
+        System.out.println(itemName + "removed from the catalog with quantity.");
+      } else {
+        System.out.println(quantity + " " + itemName + " removed from the catalog. Quantity leftover: " + quantityOfItem[i]);
+      }
+      return();
+    }
+  }
 }
     
   
