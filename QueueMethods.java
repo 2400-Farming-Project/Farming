198ba13014d926b6367073046784ef205b8a6dfd
Angel Ramos <ajralt123@gmail.com>
2024-04-22 22:34:40 -0700

    Modified QueueMethods add queue method stubs and structure

--- QueueMethods.java
+++ QueueMethods.java
@@ -1,5 +1,41 @@
 import java.util.Queue;
+import java.util.LinkedList;
 
-public class QueueMethods {
-    
+
+public class QueueMethods implements QueueInterface<T> {
+private static Queue<T> customerQueue = new LinkedList<>();
+
+    @Override
+    public void enqueue(T customer) {
+        customerQueue.add(customer);
+        System.out.println(customerQueue);
+    }
+
+    @Override
+    public T dequeue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+
+
+
 }
