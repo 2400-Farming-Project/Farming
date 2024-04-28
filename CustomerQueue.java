6db3540779dd4832ef8def208e35b887a86b12c1
boop213 <150979904+boop213@users.noreply.github.com>
2024-04-28 14:20:00 -0700

    Update CustomerQueue.java
    added File initialization for saving and uploading and implemented saveQueueFiles and uploadQueueFiles.

--- CustomerQueue.java
+++ CustomerQueue.java
@@ -26,4 +26,58 @@ public class CustomerQueue {
             }
         }
     }
+
+ public void saveQueueFiles() {
+        try {
+        if (customerQueueFile.createNewFile()) {
+    //System.out.println("customerQueueFile created");
+} else {
+//System.out.println("File already exists");
+}
+} catch (IOException e) {
+    System.out.println("An error occurred while creating the file.");
+e.printStackTrace();
+}
+    
+    try {
+        PrintStream writer = new PrintStream(customerQueueFile);
+    for (int index = 0; index < customerQueue.size(); index++) {
+        if (((LinkedList<String>) customerQueue).get(index) != null) {
+        writer.println(((LinkedList<String>) customerQueue).get(index));
+    }
+    }
+    writer.close();
+      } catch (FileNotFoundException e) {
+          System.out.println("Customer Log names not saved, error has occurred");
+      e.printStackTrace();
+      }
+    }
+
+    public void uploadQueueFiles(){
+        try {
+            if (customerQueueFile.createNewFile()) {
+              //System.out.println("customerQueueFile created");
+            } else {
+                //System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file.");
+            e.printStackTrace();
+        }
+
+        Scanner fileReader;
+        try {
+            fileReader = new Scanner(customerQueueFile);
+            while(fileReader.hasNext()){
+                String currentName = fileReader.next();
+                customerQueue.add(currentName);
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File customerQueue not found");
+            e.printStackTrace();
+        }
+        
+    }
+    
 }
